80786bd98a307d4a49937b31dcca9b7b43aecb53
zanhecht <ahecht@earthlink.net>
2021-05-27 10:39:25 -0400

    v2
    Use redis to prevent random duplicates

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
-$nicks = array("Alpaca", "Berry", "Canary", "Durian", "Emu", "Fig", "Grape", "Heron", "Ibex", "Jaguar", "Kiwi", "Lemur", "Melon", "Narwhal", "Ocelot", "Papaya", "Quetzal", "Rhino", "Stork", "Tiger", "Unicorn", "Vervet", "Walnut", "Xerinae", "Yew", "Zebra");
-$colors = array("Brown", "Cyan", "Green", "Magenta", "Orange", "Purple", "Red", "White", "Yellow");
-$nick = $colors[array_rand($colors)] . $nicks[array_rand($nicks)] . mt_rand(0,9) . mt_rand(0,9); 
+$redisClient = new Redis();
+$redisClient -> connect('tools-redis',6379);
+$num = $redisClient -> get('84Gbx4rM1qBT91Y4rbUMvuJZkJSaWETP_num');
+if($num < 99) {	$num += 1; } else {	$num = 0; }
+$redisClient -> set('84Gbx4rM1qBT91Y4rbUMvuJZkJSaWETP_num', $num);
+$redisClient -> close();
+
+$nicks = array("Alpaca", "Berry", "Canary", "Durian", "Emu", "Fig", "Grape", "Heron", "Ibex", "Jaguar", "Kiwi", "Lemur", "Melon", "Narwhal", "Ocelot", "Papaya", "Quetzal", "Rhino", "Stork", "Tiger", "Unicorn", "Vervet", "Walnut", "Xerus", "Yew", "Zebra");
+$colors = array("Amber", "Brown", "Cyan", "Diamond", "Emerald", "Green", "Indigo", "Jade", "Khaki", "Lime", "Magenta", "Navy", "Orange", "Purple", "Red", "Silver", "Tan", "White", "Yellow", "Zircon");
+$nick = $colors[$num%20] . $nicks[array_rand($nicks)] . sprintf("%02d", $num); 
 
 $channel = $_GET['channel'];
 if($channel === null) $channel = 'wikipedia-en-help';
@@ -10,5 +17,6 @@ $server = $_GET['server'];
 if($server === null) $server = 'irc.freenode.net';
 
 header('Location: https://kiwiirc.com/client/' . urlencode($server) . '/' . urlencode($channel) . '?nick=' . urlencode($nick));
+//echo('Location: https://kiwiirc.com/client/' . urlencode($server) . '/' . urlencode($channel) . '?nick=' . urlencode($nick));
 exit();
 ?>
