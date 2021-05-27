f92931e5df572e8b0b655122093f22dc39610388
zanhecht <ahecht@earthlink.net>
2021-05-27 10:39:57 -0400

    v3
    Use external source for name generation

--- index.php
+++ index.php
@@ -1,14 +1,36 @@
 <?php
 $redisClient = new Redis();
 $redisClient -> connect('tools-redis',6379);
-$num = $redisClient -> get('84Gbx4rM1qBT91Y4rbUMvuJZkJSaWETP_num');
+$num = $redisClient -> get('Bm0F0TKdeyhUE0TEO0karLN4q0G+ElXM_num');
 if($num < 99) {	$num += 1; } else {	$num = 0; }
-$redisClient -> set('84Gbx4rM1qBT91Y4rbUMvuJZkJSaWETP_num', $num);
+$redisClient -> set('Bm0F0TKdeyhUE0TEO0karLN4q0G+ElXM_num', $num);
 $redisClient -> close();
 
-$nicks = array("Alpaca", "Berry", "Canary", "Durian", "Emu", "Fig", "Grape", "Heron", "Ibex", "Jaguar", "Kiwi", "Lemur", "Melon", "Narwhal", "Ocelot", "Papaya", "Quetzal", "Rhino", "Stork", "Tiger", "Unicorn", "Vervet", "Walnut", "Xerus", "Yew", "Zebra");
-$colors = array("Amber", "Brown", "Cyan", "Diamond", "Emerald", "Green", "Indigo", "Jade", "Khaki", "Lime", "Magenta", "Navy", "Orange", "Purple", "Red", "Silver", "Tan", "White", "Yellow", "Zircon");
-$nick = $colors[$num%20] . $nicks[array_rand($nicks)] . sprintf("%02d", $num); 
+$ntoptext = "----<!--NOUN VALUES ONLY BELOW THIS LINE-->----\n";
+$nbottext = "----<!--NOUN VALUES ONLY ABOVE THIS LINE-->----\n";
+
+$atoptext = "----<!--ADJECTIVE VALUES ONLY BELOW THIS LINE-->----\n";
+$abottext = "----<!--ADJECTIVE VALUES ONLY ABOVE THIS LINE-->----\n";
+
+$baseurl = $_GET['baseurl'];
+$baseurl = ($baseurl === null) ? $baseurl = "https://en.wikipedia.org/w/index.php?title=Wikipedia:IRC_help_disclaimer/nicks&action=raw" : urldecode($baseurl);
+
+$nicks = file($baseurl);
+
+$nountop = array_search($ntoptext,$nicks);
+$nounbot = in_array($nbottext,$nicks) ? array_search($nbottext,$nicks) : array_search(rtrim($nbottext),$nicks);
+$adjtop = array_search($atoptext,$nicks);
+$adjbot = in_array($abottext,$nicks) ? array_search($abottext,$nicks) : array_search(rtrim($abottext),$nicks);
+
+if($nountop === FALSE or $nounbot === FALSE or $adjtop === FALSE or $adjbot === FALSE) {
+    $nouns = ["Help"];
+    $adjs = array_fill(0,20,"WP");
+} else {
+    $nouns = array_slice($nicks,$nountop+1,$nounbot-$nountop-1);
+    $adjs = array_pad(array_slice($nicks,$adjtop+1,$adjbot-$adjtop-1),20,"");
+}
+
+$nick = rtrim($adjs[$num%20]) . rtrim($nouns[array_rand($nouns)]) . sprintf("%02d", $num); 
 
 $channel = $_GET['channel'];
 if($channel === null) $channel = 'wikipedia-en-help';
@@ -19,4 +41,34 @@ if($server === null) $server = 'irc.freenode.net';
 header('Location: https://kiwiirc.com/client/' . urlencode($server) . '/' . urlencode($channel) . '?nick=' . urlencode($nick));
 //echo('Location: https://kiwiirc.com/client/' . urlencode($server) . '/' . urlencode($channel) . '?nick=' . urlencode($nick));
 exit();
-?>
+
+/*
+        Copyright (c) 2017 Ahecht (https://en.wikipedia.org/wiki/User:Ahecht)
+
+        The MIT License
+
+        Copyright (c) 2011 Wyss Institute at Harvard University
+        
+        Permission is hereby granted, free of charge, to any person
+        obtaining a copy of this software and associated documentation
+        files (the \"Software\"), to deal in the Software without
+        restriction, including without limitation the rights to use,
+        copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the
+        Software is furnished to do so, subject to the following
+        conditions:
+
+        The above copyright notice and this permission notice shall be
+        included in all copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND,
+        EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+        OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+        NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+        HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+        WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+        FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+        OTHER DEALINGS IN THE SOFTWARE.
+        
+        http://www.opensource.org/licenses/mit-license.php
+*/
