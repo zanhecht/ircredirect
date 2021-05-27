486b1f8183bcdfcebb527d24f26b77c3c656bc51
zanhecht <ahecht@earthlink.net>
2021-05-27 11:21:21 -0400

    v7
    Fix XSS vulnerability, only load seed file from whitelisted domains, default to libera.chat, add additional debug outputs, adjust formatting of landing page, use private redis key

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
+$redisKey = @file_get_contents('../redis.key') ?: 'Bm0F0TKdeyhUE0TEO0karLN4q0G+ElXM:num';
 $redisClient = new Redis();
 $redisClient -> connect('tools-redis',6379);
-$num = $redisClient -> get('Bm0F0TKdeyhUE0TEO0karLN4q0G+ElXM:num');
+$num = $redisClient -> get($redisKey);
 if ($num < 99) {
 	$num += 1;
 	if (($num == 69) or ($num == 88)) {
@@ -10,7 +11,7 @@ if ($num < 99) {
 } else {
 	$num = 0;
 }
-$redisClient -> set('Bm0F0TKdeyhUE0TEO0karLN4q0G+ElXM:num', $num);
+$redisClient -> set($redisKey, $num);
 $redisClient -> close();
 
 if (isset($_GET['baseurl'])) {
@@ -19,13 +20,17 @@ if (isset($_GET['baseurl'])) {
 	$baseurl = 'https://en.wikipedia.org/w/index.php?title=Wikipedia:IRC_help_disclaimer/nicks.json&action=raw';
 }
 
-$jsonFile = file_get_contents($baseurl);
+if (preg_match("/^https:\/\/[^\/]*\.?(mediawiki|toolforge|wik(i(books|data|[mp]edia|news|quote|source|versity|voyage)|tionary)).org\//i", $baseurl)) {
+	$jsonFile = file_get_contents($baseurl);
+} else {
+	$jsonFile = FALSE;
+}
 
 if ($jsonFile) {
 	$nicks = json_decode($jsonFile, TRUE);
 }
 
-if ($jsonFile === FALSE or $nicks === FALSE or (is_array($nicks) and (!array_key_exists('nouns', $nicks) or !array_key_exists('adjectives', $nicks)))) {
+if ($jsonFile === FALSE or $nicks === FALSE or is_array($nicks) === FALSE or (is_array($nicks) and (!array_key_exists('nouns', $nicks) or !array_key_exists('adjectives', $nicks)))) {
 	$nick = 'WPHelp' . sprintf('%02d', $num);
 } else {
 	$nick = $nicks['adjectives'][$num%20] . $nicks['nouns'][array_rand($nicks['nouns'])] . sprintf('%02d', $num);
@@ -40,28 +45,32 @@ if (isset($_GET['channel'])) {
 if (isset($_GET['server'])) {
 	$server = $_GET['server'];
 } else {
-	$server = 'irc.freenode.net';
+	$server = 'irc.libera.chat';
 }
 
 $targetURL = 'https://kiwiirc.com/nextclient/' . urlencode($server) . '/' . urlencode($channel) . '?nick=' . urlencode($nick);
 
 if (isset($_GET['debug'])) {
-	echo("Location: $targetURL<hr>");
+	echo('Location: '.htmlspecialchars($targetURL).'<hr>');
+	echo('Server: '.htmlspecialchars($server).'<hr>');
+	echo('Channel: '.htmlspecialchars($channel).'<hr>');
+	echo('BaseUrl: '.htmlspecialchars($baseurl).'<hr>');
 	if ($jsonFile === FALSE) {
 		echo('$jsonFile === FALSE<hr>');
 	} else {
-		echo("\$jsonFile = $jsonFile<hr>");
+		echo('$jsonFile = '.htmlspecialchars($jsonFile).'<hr>');
 	}
 	if ($nicks === FALSE) {
 		echo('$nicks === FALSE<hr>');
 	} elseif (is_array($nicks)) {
 		echo('is_array($nicks) === TRUE, array_key_exists(\'nouns\', $nicks) = ' . (array_key_exists('nouns', $nicks) ? 'TRUE' : 'FALSE') . ', array_key_exists(\'adjectives\', $nicks) = ' . (array_key_exists('adjectives', $nicks)  ? 'TRUE' : 'FALSE') . '<hr>'); 
 	} else {
-		echo("is_array(\$nicks) === FALSE, \$nicks = $nicks<hr>");
+		echo('is_array($nicks) === FALSE, $nicks = '.htmlspecialchars($nicks).'<hr>');
 	}
 } elseif (isset($_GET['consent'])) {
 	header("Location: $targetURL");
 } else {
+	$chanhtml = htmlspecialchars($channel).' on '.htmlspecialchars($server);
 	echo("<!DOCTYPE html>
 		<html lang='en'>
 			<head>
@@ -69,10 +78,14 @@ if (isset($_GET['debug'])) {
 				<link rel='StyleSheet' href='resources/mediawiki.ui.button.css' type='text/css' media='screen' />
 				<style type='text/css'>
 					<!--
-					body {font-size:90%; font-family: sans-serif;}
-					h1 {margin:.8em 0 .2em 0;padding:0;}
+					body {font-family: sans-serif; background-color: #ffffff; color: #202122; padding: 1em; font-size: 0.875em; line-height: 1.6;}
+					h1, h2, h3, h4, h5, h6 {color: #000; margin: 0;}
+					h1, h2 {margin-bottom: 0.25em; padding: 0; font-family: 'Linux Libertine','Georgia','Times',serif; line-height: 1.3; border-bottom: 1px solid #a2a9b1;}
+					h1 {font-size: calc(1.8em/0.875); font-weight: normal;}
 					p {margin:.4em 0 .8em 0;padding:0;}
-					code {font-family: monospace !important;}
+					code {border-radius: 2px; padding: 1px 4px;}
+					pre, code {background-color: #f8f9fa; color: #000; border: 1px solid #eaecf0;}
+					pre, code, tt, kbd, samp {font-family: monospace,monospace;}
 					a {text-decoration: none; color: #0645ad;}
 					a:hover, a:focus {text-decoration: underline;}
 					a:visited {color:#0b0080}
@@ -86,7 +99,7 @@ if (isset($_GET['debug'])) {
 				<p style='text-align: center;'>Click on the button below to continue to <a href=$targetURL>$targetURL</a>.<br />
 				This is a third-party site not run by the Wikimedia Foundation, and may have a different privacy policy.</p>
 				<p style='text-align: center;'><a href='$targetURL'>
-					<span class='mw-ui-button mw-ui-progressive'>Continue to #$channel at kiwiirc</span>
+					<span class='mw-ui-button mw-ui-progressive'>Continue to #$chanhtml at kiwiirc</span>
 				</a></p>
 				<p style='text-align: center;'>&nbsp;</p>
 				<p style='text-align: center; font-size: smaller;'>To avoid seeing this message in the future, add <code>&consent=yes</code> to the end of the URL. <a href='README.html'>View documentation</a>.</p>
@@ -99,7 +112,7 @@ exit();
 /*
 		The MIT License (MIT)
 
-		Copyright (c) 2017–2019 Ahecht (https://en.wikipedia.org/wiki/User:Ahecht)
+		Copyright (c) 2017–2021 Ahecht (https://en.wikipedia.org/wiki/User:Ahecht)
 
 		Permission is hereby granted, free of charge, to any person
 		obtaining a copy of this software and associated documentation
