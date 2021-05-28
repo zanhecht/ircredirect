6bf729f377c3a0355cfba13f33611c57fdde5dbd
zanhecht <ahecht@earthlink.net>
2021-05-28 12:30:28 -0400

    v8.1
    Only specify default project once

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ if (isset($_GET['channel'])) {
 } elseif (isset($_GET['lang']) or isset($_GET['project'])) {
 	$channel = "$project-$lang";
 } else {
-	$channel = 'wikipedia';
+	$channel = $project;
 }
 
 if (isset($_GET['server'])) {
